f961d86187b718862034c73744e0e9abc062bd10
Abhishek Kumar <abhishekdatapur@gmail.com>
2026-03-15 00:23:30 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,15 @@ try {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({
-    origin: [ "http://localhost:5173",  "http://localhost:5173"],
-    credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://blog-app-kohl-omega-45.vercel.app"
+    ],
+    credentials: true
+  })
+);
 
 app.use(fileUpload({
     useTempFiles: true,
