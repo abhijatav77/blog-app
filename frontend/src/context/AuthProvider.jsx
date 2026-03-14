f7933b5fff56b2a7eeeda2855a8494fb00983dff
Abhishek Kumar <abhishekdatapur@gmail.com>
2026-03-14 23:26:39 +0530

    change

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -11,7 +11,6 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    if(!isAuthenticated) return;
     const fetchProfile = async () => {
       try {
           const { data } = await axios.get(`${BACKEND_URL}/users/my-profile`, {
